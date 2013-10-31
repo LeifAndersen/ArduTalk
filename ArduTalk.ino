dc4270ff06ab9cff0aa0fd5604ffa4822305f469
Leif Andersen <leif@leifandersen.net>
2013-10-30 23:10:30 -0600

    First expirement working.

--- ArduTalk.ino
+++ ArduTalk.ino
@@ -6,11 +6,11 @@
 #ifdef ARD_1
 #define SERVER_PORT 10002
 #define CLIENT_PORT 10003
-char *message = "I'm ARD_1!\n";
+//char *message = "I'm ARD_1!\n";
 #else
 #define SERVER_PORT 10003
 #define CLIENT_PORT 10002
-char *message = "I'm ARD_2!\n";
+//char *message = "I'm ARD_2!\n";
 #endif
 
 byte mac[] = { 
@@ -30,16 +30,17 @@ IPAddress otherIP(192,168,0,177);
 #endif
 
 EthernetServer server(SERVER_PORT);
-boolean alreadyConnected = false;
 
 EthernetClient client;
+
+long timer = 0;
 
 void setup()
 {
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
   Serial.begin(9600);
-  while(!Serial) { 
+  while(!Serial) {
   }
 
   delay(1000);
@@ -61,35 +62,34 @@ void loop()
 {
   EthernetClient otherClient = server.available();
 
-  if(otherClient) {
-    if(!alreadyConnected) {
-      otherClient.flush();
-      Serial.println("We have a new client.");
-      otherClient.println("Hello client!");
-      alreadyConnected = true;
-    }
-
-    if(otherClient.available()) {
-      char thisChar = otherClient.read();
-      server.write(thisChar);
-      Serial.write(thisChar);
-    }
-  }
-
   if(client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  while (Serial.available() > 0) {
-    char inChar = Serial.read();
-    if (client.connected()) {
-      client.print(inChar); 
+    Serial.println("Received Text:");
+    while(client.available()) {
+      char c = client.read();
+      Serial.print(c);
+      if(c == 'a') {
+        long timer2 = millis();
+        Serial.print("Ping Delay of:");
+        Serial.print(timer2-timer);
+        Serial.println("ms");
+      }
     }
+    Serial.println();
   }
 
-  if(client.connected()) {
-    client.print(message);
+  if(Serial.available() > 0) {
+    Serial.print("Sending:");
+    while (Serial.available() > 0) {
+      char inChar = Serial.read();
+      if (client.connected()) {
+        client.print(inChar);
+        Serial.print(inChar);
+        if(inChar == 'a') {
+          timer = millis();
+        }
+      }
+    }
+    Serial.println();
   }
 
   if (!client.connected()) {
@@ -97,6 +97,14 @@ void loop()
     Serial.println("disconnecting.");
     client.stop();
     while(true);
+  }
+  
+  if(otherClient) {
+    while(otherClient.available()) {
+      char thisChar = otherClient.read();
+      server.write(thisChar);
+      Serial.write(thisChar);
+    }
   }
 }
 
